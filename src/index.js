7e3a4633d5d60b2a3bc780b10b0bb0aba9942c06
Suryansh Bhati <suryanshbhati20@gmail.com>
2026-03-02 19:27:56 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -6,4 +6,17 @@ dotenv.config({
     path: "./env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error in server !!!", error);
+        throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+.catch((err) => {
+    console.log("connection failed !!!", err);
+});
