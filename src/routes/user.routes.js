a6ae5661dcf7a1447dbffc4eab0280d2e8411d23
Suryansh Bhati <suryanshbhati20@gmail.com>
2026-03-10 19:27:52 +0530

    add login and logout controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -8,5 +9,10 @@ router.route("/register").post(upload.fields([
     { name: "avatar", maxCount: 1 },
     { name: "coverImage", maxCount: 1 }
 ]),registerUser);
+
+router.route("/login").post(loginUser);
+
+//Secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
